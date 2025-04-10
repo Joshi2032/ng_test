0da9e8c511235c59fe3a55c5a37bf1cdf0663fdd
Joshi <joshi.jc2032@gmail.com>
2025-04-10 09:12:27 -0600

    project base

---
+++ src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch:'full'
+  },
+  {
+    path: 'homme',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    component: ErrorComponent
+  }
+];
